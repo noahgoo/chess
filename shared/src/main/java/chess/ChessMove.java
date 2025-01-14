788bb06ceb5839fb609a6f52b60267cb311025b9
noahgoo <noahgoo@byu.edu>
2025-01-14 11:10:04 -0700

    Override Hashcode, equal and toString

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -7,23 +9,27 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
+        private ChessPosition startPosition;
+        private ChessPosition endPosition;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return endPosition;
     }
 
     /**
@@ -34,5 +40,27 @@ public class ChessMove {
      */
     public ChessPiece.PieceType getPromotionPiece() {
         throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public String toString() {
+        return "ChessMove{" +
+                "startPosition=" + startPosition +
+                ", endPosition=" + endPosition +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition);
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,8 +11,34 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private ChessGame.TeamColor teamColor;
+    private ChessPiece.PieceType pieceType;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.teamColor = pieceColor;
+        this.pieceType = type;
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "teamColor=" + teamColor +
+                ", pieceType=" + pieceType +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return teamColor == that.teamColor && pieceType == that.pieceType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamColor, pieceType);
     }
 
     /**
@@ -25,18 +53,19 @@ public class ChessPiece {
         PAWN
     }
 
+
     /**
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return teamColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+         return pieceType;
     }
 
     /**
@@ -47,6 +76,36 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        PieceType piece = board.getPiece(myPosition).getPieceType();
+        if (piece.equals(PieceType.PAWN)) {
+            Collection<ChessPosition> potentialMoves = PawnMoves(board, myPosition);
+            Collection<ChessMove> validMoves = new ArrayList<>();
+            for (ChessPosition potentialMove : potentialMoves) {
+                validMoves.add(new ChessMove(myPosition, potentialMove, null));
+            }
+            return validMoves;
+        }
         throw new RuntimeException("Not implemented");
+    }
+
+    public Collection<ChessPosition> PawnMoves(ChessBoard board, ChessPosition myPosition) {
+        // create an array for possible moves and determine if piece is white or black
+        Collection<ChessPosition> moves = new ArrayList<>();
+        ChessPiece piece = board.getPiece(myPosition);
+        int movement;
+        if (piece.getTeamColor().equals(ChessGame.TeamColor.WHITE)) {
+
+        }
+        if (board.getPiece(new ChessPosition(myPosition.getRow()+1, myPosition.getColumn())) == null) {
+            moves.add(new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()));
+
+        } else if (board.getPiece(new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()+1 )) != null) {
+            moves.add(new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()+1 ));
+        }
+        return moves;
+    }
+
+    {
+
     }
 }
