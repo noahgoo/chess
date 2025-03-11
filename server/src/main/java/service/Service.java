fb2cf2ef310a5647def06c175c1e299345287aa8
noahgoo <noahgoo@byu.edu>
2025-03-11 10:30:48 -0600

    Got rid of unused statment.

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -6,5 +6,4 @@ public class Service {
     public static final UserDAO USER_DAO = new SQLUserDAO();
     public static final AuthDAO AUTH_DAO = new SQLAuthDAO();
     public static final GameDAO GAME_DAO = new SQLGameDAO();
-    public static final DAO DAO = new DAO();
 }
