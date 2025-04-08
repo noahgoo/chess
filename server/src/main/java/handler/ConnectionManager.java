ef0aedf81c795d28d1528b390552a87b990f4b25
noahgoo <noahgoo@byu.edu>
2025-04-08 14:37:54 -0600

    Redid my ConnectionManager so that it works with a better HashMap and also so broadcast works properly.

--- server/src/main/java/handler/ConnectionManager.java
+++ server/src/main/java/handler/ConnectionManager.java
@@ -6,34 +6,48 @@ import websocket.messages.ServerMessage;
 
 import javax.imageio.IIOException;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+    public final ConcurrentHashMap<Integer, Set<Connection>> connections = new ConcurrentHashMap<>();
 
     public void add(UserGameCommand command, Session session, String username) {
-        var conn = new Connection(command.getAuthToken(), command.getGameID(), session, username);
-        connections.put(command.getAuthToken(), conn);
+        Integer gameID = command.getGameID();
+        var conn = new Connection(command.getAuthToken(), gameID, session, username);
+        if (connections.contains(gameID)) {
+            connections.get(gameID).add(conn);
+        } else {
+            connections.put(gameID, new HashSet<>(List.of(conn)));
+        }
     }
 
     public void remove(UserGameCommand command) {
-        connections.remove(command.getAuthToken());
+        Set<Connection> set = connections.get(command.getGameID());
+        for (Connection conn: set) {
+            if (conn.authToken.equals(command.getAuthToken())) {
+                connections.get(command.getGameID()).remove(conn);
+            }
+        }
+        if (set.isEmpty()) {
+            connections.remove(command.getGameID());
+        }
     }
 
-    public void broadcast(String username, ServerMessage type, String message) throws IOException {
-        var removalList = new ArrayList<>();
-        for (var c: connections.values()) {
+    public void broadcast(UserGameCommand command, String message) throws IOException {
+        ArrayList<Connection> removalList = new ArrayList<>();
+        Set<Connection> set = connections.get(command.getGameID());
+        for (Connection c: set) {
             if (c.session.isOpen()) {
-                if (!c.username.equals(username)) {
+                if (!c.authToken.equals(command.getAuthToken())) {
                     c.session.getRemote().sendString(message);
                 }
             } else {
                 removalList.add(c);
             }
         }
-        for (var c: removalList) {
-            connections.remove(c);
+        for (Connection c: removalList) {
+            connections.get(command.getGameID()).remove(c);
         }
     }
 }
